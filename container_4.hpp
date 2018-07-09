b449fa90caee3eafb58976b7bf7df5656d7118f0
Alex <lagius@grief.ly>
2018-07-09 15:39:49 +0300

    Container 4 optimization

--- container_4.hpp
+++ container_4.hpp
@@ -57,7 +57,7 @@ public:
         : data_(p.data_ ? new int{*p.data_} : nullptr)
     {}
 
-    my_unique_ptr_opt(my_unique_ptr_opt&& p)
+    my_unique_ptr_opt(my_unique_ptr_opt&& p) noexcept
         : data_(p.data_)
     {
         p.data_ = nullptr;
